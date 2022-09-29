9a9846c229639a54820da1e12513ac402f1f3cda
Andreas Pizsa <hopster@gmail.com>
2022-09-29 21:16:41 +0200

    feat: add segment.com

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
+  <script>
+    !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="SMC2GLN3sTWcQVlsxG6VtLi9NLwzhhll";;analytics.SNIPPET_VERSION="4.15.3";
+    analytics.load("SMC2GLN3sTWcQVlsxG6VtLi9NLwzhhll");
+    analytics.page();
+    }}();
+  </script>
   <meta name="theme-color" content="#0ea3e0">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $site->title() ?> | ★★★★★ · diese genialen, klimafreundlichen Waschmittel Blätter kommen zu Dir in den Briefkasten · Kein Plastik, kein Schleppen!</title>
+  <title><?= $site->title() ?> ★★★★★ · Das klimafreundliche, plastikfreie Waschmittel in Papierform, das zu Dir in den Briefkasten kommt · Kein Plastik, kein Schleppen!</title>
   <?= vite()->client() ?>
   <?= vite()->css() ?>
   
